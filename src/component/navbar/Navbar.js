6417f62b03a78bdf9725f33697acac3d50ec448c
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-06-13 12:45:01 +0530

    navbar-component-create

---
+++ src/component/navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+          <div className="Navbar">
+              <p>navbar</p>
+      </div>
+    </div>
+  )
+}
--- src/home/Home.js
+++ src/home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.scss'
 import AcUnitIcon from '@mui/icons-material/AcUnit';
+import Navbar from '../component/navbar/Navbar';
+
 export default function Home() {
   return (
     <div>
           <div className="Home">
-              <AcUnitIcon/>
+             <Navbar/>
       </div>
     </div>
   )
