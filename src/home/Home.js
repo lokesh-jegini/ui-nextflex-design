bc6e1a411e8257f0efd68f51feee85cfd7e0e502
lokesh jegini <jegini.lokesh@integra-india.co.in>
2023-06-14 14:39:07 +0530

    fearure component info image commit

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,14 +2,14 @@ import React from 'react'
 import './Home.scss'
 import AcUnitIcon from '@mui/icons-material/AcUnit';
 import Navbar from '../component/navbar/Navbar';
+import Featured from '../component/featured/Featured';
 
 export default function Home() {
   return (
     
           <div className="Home">
         <Navbar />
-        <img src="https://media.istockphoto.com/id/1341408852/video/colored-smoke-on-a-dark-background-blue-and-red-light-with-smoke.jpg?s=640x640&k=20&c=v2DQUY8IVbli_6FH_9KAs6YWRXlDdYiBJHfp7JFh7NY=" alt="" />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, distinctio perspiciatis. Cum eius ullam itaque saepe explicabo aut nam at libero, provident delectus non, voluptate facere corporis voluptates culpa eligendi, voluptatibus molestias ducimus veritatis iusto. Id quos fuga magni a mollitia, repellendus excepturi architecto esse laborum atque, rem, temporibus modi quod ducimus explicabo ex numquam natus eum aspernatur aperiam. Ipsum voluptatem distinctio et aliquid ullam totam, debitis odio possimus temporibus pariatur vitae at odit est quibusdam nam impedit harum tenetur explicabo tempora? Quam unde repellat harum hic pariatur inventore aliquid, itaque eum necessitatibus? Ratione quod autem, fuga consequatur aperiam mollitia cupiditate est! Itaque quam ab illum doloribus quos deserunt necessitatibus quasi explicabo molestiae voluptatem, qui repudiandae neque aut temporibus ex in inventore blanditiis harum nam hic quibusdam accusantium at? Repellat nostrum tenetur nam rerum dolore error quibusdam vitae, expedita dolorem, earum fuga? Ipsa numquam eos sequi commodi voluptatem eum minus voluptatum rem sapiente, maiores vel magni natus aspernatur adipisci debitis et fugiat quibusdam porro voluptate cumque harum. Id a sequi neque facilis rem saepe ullam quae? Maiores alias sapiente dolores commodi error quas, nam iure earum laborum culpa neque, cupiditate sit harum dolorem accusantium! Quod earum accusamus consequuntur itaque dicta!
+        <Featured />
     </div>
   )
 }
